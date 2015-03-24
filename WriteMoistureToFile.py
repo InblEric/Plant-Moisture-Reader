5f3ece6dd68601b371d80bf54e20c924f877b9ee
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2015-03-24 17:17:07 -0500

    Flush data to file every time so we don't lose any if we crash

--- WriteMoistureToFile.py
+++ WriteMoistureToFile.py
@@ -11,3 +11,4 @@ def sendData(data):
     ts = str(curtime.tm_year) + "-" + str(curtime.tm_mon) + "-" + str(curtime.tm_mday) + ":" + str(curtime.tm_hour) + "-" + str(curtime.tm_min) + "-" + str(curtime.tm_sec)
         
     f.write(str(data) + ", " + str(ts) + "\n")
+    f.flush()
